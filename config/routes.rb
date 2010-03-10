ce8a037c904c5c23b336b5b8e91cc0526c5650ed
r32code <r32.code@gmail.com>
2010-03-10 08:46:56 +0000

    change view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -40,4 +41,10 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  
+  
+  
+  # Mapping to home directory
+  map.root :controller => 'home', :action => 'show'
 end
